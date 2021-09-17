26a5e169ff394e11079164320355f153496fdeec
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-18 01:31:40 +0300

    Add action observer

---
+++ Assets/Observer/Action/Scripts/Clickable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.Observer.Action
+{
+    internal class Clickable : MonoBehaviour
+    {
+        public static Action<GameObject, Vector3> OnSomeoneClickedOnMe;
+
+        private void OnMouseDown()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitData;
+
+            if (Physics.Raycast(ray, out hitData, 1000))
+            {
+                SomeoneClickedOnMe(hitData.point);
+            }
+        }
+
+        private void SomeoneClickedOnMe(Vector3 mouseLocalPositionOnMe)
+        {
+            OnSomeoneClickedOnMe?.Invoke(gameObject, mouseLocalPositionOnMe);
+        }
+    }
+}
+
---
+++ Assets/Observer/Action/Scripts/MouseHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.Observer.Action
+{
+    internal class MouseHandler : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Clickable.OnSomeoneClickedOnMe += OnNotify;
+        }
+
+        private void OnDisable()
+        {
+            Clickable.OnSomeoneClickedOnMe -= OnNotify;
+        }
+
+        private void OnNotify(GameObject obj, Vector3 mouseLocalPositionOnSubject)
+        {
+            Debug.Log("I am reporting as " + gameObject.name + ". Someone touched the " + mouseLocalPositionOnSubject.ToString() + " of " + obj.name + ".");
+        }
+    }
+}
