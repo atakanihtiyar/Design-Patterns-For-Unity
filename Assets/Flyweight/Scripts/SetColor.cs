b90e86d3f5b3573a915eb7b033bd27240f6d5448
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-18 08:52:41 +0300

    Add flyweight

---
+++ Assets/Flyweight/Scripts/SetColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.Flyweight
+{
+    internal class SetColor : MonoBehaviour
+    {
+        private Renderer _renderer;
+        private MaterialPropertyBlock _propBlock;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer>();
+            _propBlock = new MaterialPropertyBlock();
+        }
+
+        private void Update()
+        {
+            _renderer.material.color = GetRandomColor();
+        }
+
+        private Color GetRandomColor()
+        {
+            return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+}
+
---
+++ Assets/Flyweight/Scripts/SetColorMPB.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.Flyweight 
+{
+    internal class SetColorMPB : MonoBehaviour
+    {
+        private Renderer _renderer;
+        private MaterialPropertyBlock _propBlock;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer>();
+            _propBlock = new MaterialPropertyBlock();
+        }
+
+        private void Update()
+        {
+            _renderer.GetPropertyBlock(_propBlock);
+            _propBlock.SetColor("_Color", GetRandomColor());
+            _renderer.SetPropertyBlock(_propBlock);
+        }
+
+        private Color GetRandomColor()
+        {
+            return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+}
+
