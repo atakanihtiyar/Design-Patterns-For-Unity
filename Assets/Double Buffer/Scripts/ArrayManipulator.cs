8d6d8b7b2b77f1db326d8848e27588520c845cfd
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-26 19:39:11 +0300

    Updated double buffer usage

--- Assets/Double Buffer/Scripts/ArrayManipulator.cs
+++ Assets/Double Buffer/Scripts/ArrayManipulator.cs
@@ -18,17 +18,18 @@ namespace DesignPatterns.DoubleBuffer
         /// <param name="fromData"> Array for manipulate information </param>
         /// <param name="applyToCell"> Manipulate function. Function parameters must be data array value, another array value, cell x index, cell y index. Return must be new manipulated cell value. </param>
         /// <returns>Returns manipulated array data </returns>
-        public static ArrayType1[,] ManipulateFromAnother<ArrayType1, ArrayType2>(ArrayType1[,] data, ArrayType2[,] fromData, System.Func<ArrayType1, ArrayType2, int, int, ArrayType1> applyToCell)
+        public static ArrayType1[,] ManipulateFromAnother<ArrayType1, ArrayType2>(ArrayType1[,] data, ArrayType2[,] fromData, System.Func<ArrayType1, ArrayType2, int, int, ArrayType1> applyToCell) where ArrayType1 : new()
         {
+            ArrayType1[,] newData = new ArrayType1[data.GetLength(0), data.GetLength(1)];
             for (int y = 0; y < data.GetLength(0); y++)
             {
                 for (int x = 0; x < data.GetLength(1); x++)
                 {
-                    data[x, y] = applyToCell.Invoke(data[x, y], fromData[x, y], x, y);
+                    newData[x, y] = applyToCell.Invoke(data[x, y], fromData[x, y], x, y);
                 }
             }
 
-            return data;
+            return newData;
         }
 
         /// <summary>
@@ -38,17 +39,18 @@ namespace DesignPatterns.DoubleBuffer
         /// <param name="data"> Array to manipulate </param>
         /// <param name="applyToCell"> Manipulate function. Function parameters must be array value, cell x index, cell y index. Return must be new manipulated cell value. </param>
         /// <returns>Returns manipulated array data </returns>
-        public static T[,] Manipulate<T>(T[,] data, System.Func<T, int, int, T> applyToCell)
+        public static ArrayType[,] Manipulate<ArrayType>(ArrayType[,] data, System.Func<ArrayType, int, int, ArrayType> applyToCell) where ArrayType : new()
         {
+            ArrayType[,] newData = new ArrayType[data.GetLength(0), data.GetLength(1)];
             for (int y = 0; y < data.GetLength(0); y++)
             {
                 for (int x = 0; x < data.GetLength(1); x++)
                 {
-                    data[x, y] = applyToCell.Invoke(data[x, y], x, y);
+                    newData[x, y] = applyToCell.Invoke(data[x, y], x, y);
                 }
             }
 
-            return data;
+            return newData;
         }
 
         /// <summary>
@@ -60,18 +62,19 @@ namespace DesignPatterns.DoubleBuffer
         /// <param name="fromData"> 2D array for manipulate information </param>
         /// <param name="applyToCell"> Manipulate function. Function parameters must be data array value, another array value, cell x index, cell y index. Return must be new manipulated cell value. </param>
         /// <returns>Returns manipulated array data </returns>
-        public static T1[] ManipulateFrom2DTo1D<T1, T2>(T1[] data, T2[,] fromData, System.Func<T1, T2, int, int, T1> applyToCell)
+        public static ArrayType1[] ManipulateFrom2DTo1D<ArrayType1, ArrayType2>(ArrayType1[] data, ArrayType2[,] fromData, System.Func<ArrayType1, ArrayType2, int, int, ArrayType1> applyToCell) where ArrayType1 : new()
         {
+            ArrayType1[] newData = new ArrayType1[data.Length];
             for (int y = 0; y < fromData.GetLength(1); y++)
             {
                 for (int x = 0; x < fromData.GetLength(0); x++)
                 {
                     int index = y * fromData.GetLength(0) + x;
-                    data[index] = applyToCell.Invoke(data[index], fromData[x, y], x, y);
+                    newData[index] = applyToCell.Invoke(data[index], fromData[x, y], x, y);
                 }
             }
 
-            return data;
+            return newData;
         }
     }
 }
