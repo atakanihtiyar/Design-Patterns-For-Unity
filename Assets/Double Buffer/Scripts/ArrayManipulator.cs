53a51a6ea94b1616816fe64975de6bc1d3ae9627
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-26 19:12:27 +0300

    Add double buffer

---
+++ Assets/Double Buffer/Scripts/ArrayManipulator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.DoubleBuffer
+{
+    /// <summary>
+    /// Class for manipulate arrays
+    /// </summary>
+    public static class ArrayManipulator
+    {
+        /// <summary>
+        /// Function for manipulate your array data with value from another array
+        /// </summary>
+        /// <typeparam name="ArrayType1"> Array type to manipulate </typeparam>
+        /// <typeparam name="ArrayType2"> Array type for manipulate information </typeparam>
+        /// <param name="data"> Array to manipulate </param>
+        /// <param name="fromData"> Array for manipulate information </param>
+        /// <param name="applyToCell"> Manipulate function. Function parameters must be data array value, another array value, cell x index, cell y index. Return must be new manipulated cell value. </param>
+        /// <returns>Returns manipulated array data </returns>
+        public static ArrayType1[,] ManipulateFromAnother<ArrayType1, ArrayType2>(ArrayType1[,] data, ArrayType2[,] fromData, System.Func<ArrayType1, ArrayType2, int, int, ArrayType1> applyToCell)
+        {
+            for (int y = 0; y < data.GetLength(0); y++)
+            {
+                for (int x = 0; x < data.GetLength(1); x++)
+                {
+                    data[x, y] = applyToCell.Invoke(data[x, y], fromData[x, y], x, y);
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Function for manipulate your array data
+        /// </summary>
+        /// <typeparam name="ArrayType1"> Array type to manipulate </typeparam>
+        /// <param name="data"> Array to manipulate </param>
+        /// <param name="applyToCell"> Manipulate function. Function parameters must be array value, cell x index, cell y index. Return must be new manipulated cell value. </param>
+        /// <returns>Returns manipulated array data </returns>
+        public static T[,] Manipulate<T>(T[,] data, System.Func<T, int, int, T> applyToCell)
+        {
+            for (int y = 0; y < data.GetLength(0); y++)
+            {
+                for (int x = 0; x < data.GetLength(1); x++)
+                {
+                    data[x, y] = applyToCell.Invoke(data[x, y], x, y);
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Function for manipulate your 1D array data with value from another 2D array
+        /// </summary>
+        /// <typeparam name="ArrayType1"> Array type to manipulate </typeparam>
+        /// <typeparam name="ArrayType2"> Array type for manipulate information </typeparam>
+        /// <param name="data"> 1D array to manipulate </param>
+        /// <param name="fromData"> 2D array for manipulate information </param>
+        /// <param name="applyToCell"> Manipulate function. Function parameters must be data array value, another array value, cell x index, cell y index. Return must be new manipulated cell value. </param>
+        /// <returns>Returns manipulated array data </returns>
+        public static T1[] ManipulateFrom2DTo1D<T1, T2>(T1[] data, T2[,] fromData, System.Func<T1, T2, int, int, T1> applyToCell)
+        {
+            for (int y = 0; y < fromData.GetLength(1); y++)
+            {
+                for (int x = 0; x < fromData.GetLength(0); x++)
+                {
+                    int index = y * fromData.GetLength(0) + x;
+                    data[index] = applyToCell.Invoke(data[index], fromData[x, y], x, y);
+                }
+            }
+
+            return data;
+        }
+    }
+}
+
---
+++ Assets/Double Buffer/Scripts/CaveGenerator.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.DoubleBuffer
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    internal class CaveGenerator : MonoBehaviour
+    {
+        #region Fields
+
+        [Header("Display cave")]
+        private MeshRenderer caveDisplayRenderer;
+
+        [Header("Grid")]
+        [SerializeField] private int gridSize = 100;
+        private int[,] bufferOld;
+        private int[,] bufferNew;
+
+        [Header("Randomize Options")]
+        [Range(0, 100)]
+        [SerializeField] private float randomFillPercent;
+        [SerializeField] private string seed;
+        [SerializeField] private bool useSeed;
+
+
+        [Header("Smooth Options")]
+        [SerializeField] private int smoothSteps = 20;
+        [SerializeField] private float pauseTime = 1f;
+
+        [Header("Smooth Coroutine")]
+        private Coroutine smoothCoroutine = null;
+        private bool isSmoothCoroutineRun = false;
+
+        #endregion
+
+        #region Unity Lifecycle Methods
+
+        private void Start()
+        {
+            caveDisplayRenderer = GetComponent<MeshRenderer>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1")) //new cave
+            {
+                if (smoothCoroutine != null && isSmoothCoroutineRun == true)
+                {
+                    StopCoroutine(smoothCoroutine);
+                    isSmoothCoroutineRun = false;
+                }
+
+                GenerateEmptyCave();
+                RandomFillCave();
+                DisplayCave(bufferOld);
+            }
+            else if (Input.GetButtonDown("Fire2")) //smooth cave
+            {
+                if (smoothCoroutine == null || isSmoothCoroutineRun == false)
+                    smoothCoroutine = StartCoroutine(SmoothCave());
+            }
+        }
+
+        #endregion
+
+        #region Cave Generation and Display Methods
+
+        private void GenerateEmptyCave()
+        {
+            bufferOld = new int[gridSize, gridSize];
+            bufferNew = new int[gridSize, gridSize];
+        }
+
+        private void RandomFillCave()
+        {
+            if (useSeed)
+            {
+                Random.InitState(seed.GetHashCode());
+            }
+            bufferOld = ArrayManipulator.Manipulate(bufferOld, GetRandomCellValue);
+        }
+
+        private void DisplayCave(int[,] data)
+        {
+            Resources.UnloadUnusedAssets();
+
+            Texture2D texture = new Texture2D(gridSize, gridSize);
+            texture.filterMode = FilterMode.Point;
+
+            Color[] textureColors = new Color[gridSize * gridSize];
+            textureColors = ArrayManipulator.ManipulateFrom2DTo1D<Color, int>(textureColors, data, GetTextureFromValue);
+
+            texture.SetPixels(textureColors);
+            texture.Apply();
+
+            caveDisplayRenderer.sharedMaterial.mainTexture = texture;
+        }
+
+        #endregion
+
+        #region Cellular Automata Methods
+
+        private IEnumerator SmoothCave()
+        {
+            isSmoothCoroutineRun = true;
+
+            for (int i = 0; i < smoothSteps; i++)
+            {
+                RunCellularAutomataStep();
+
+                DisplayCave(bufferNew);
+
+                (bufferOld, bufferNew) = (bufferNew, bufferOld);
+
+                yield return new WaitForSeconds(pauseTime);
+            }
+
+            isSmoothCoroutineRun = false;
+            Debug.Log("Simulation completed");
+        }
+
+        private void RunCellularAutomataStep()
+        {
+            bufferNew = ArrayManipulator.ManipulateFromAnother(bufferNew, bufferOld, CellularAutomataCellProcess);
+        }
+
+        private int GetSurroundingWallCount(int cellX, int cellY)
+        {
+            int wallCount = 0;
+
+            for (int neighborX = cellX - 1; neighborX <= cellX + 1; neighborX++)
+            {
+                for (int neighborY = cellY - 1; neighborY <= cellY + 1; neighborY++)
+                {
+                    if (neighborX == cellX && neighborY == cellY)
+                    {
+                        continue;
+                    }
+
+                    if (bufferOld[neighborX, neighborY] == 1)
+                    {
+                        wallCount++;
+                    }
+                }
+            }
+
+            return wallCount;
+        }
+
+        #endregion
+
+        #region Cell Processes
+
+        private int GetRandomCellValue(int cellValue, int cellX, int cellY)
+        {
+            if (cellX == 0 || cellX == gridSize - 1 || cellY == 0 || cellY == gridSize - 1)
+            {
+                cellValue = 1;
+            }
+            else
+            {
+                cellValue = Random.Range(0f, 100f) < randomFillPercent ? 1 : 0;
+            }
+            return cellValue;
+        }
+
+        private Color GetTextureFromValue(Color cellValue, int fromCellValue, int cellX, int cellY)
+        {
+            cellValue = fromCellValue == 1 ? Color.black : Color.white;
+            return cellValue;
+        }
+
+        private int CellularAutomataCellProcess(int cellValue, int fromCellValue, int cellX, int cellY)
+        {
+            if (cellX == 0 || cellX == gridSize - 1 || cellY == 0 || cellY == gridSize - 1)
+            {
+                return 1;
+            }
+
+            int surroundingWalls = GetSurroundingWallCount(cellX, cellY);
+            if (surroundingWalls > 4)
+            {
+                return 1;
+            }
+            else if (surroundingWalls == 4)
+            {
+                return fromCellValue;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        #endregion
+    }
+}
+
